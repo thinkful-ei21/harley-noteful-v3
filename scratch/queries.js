56cb681dc7e6954372aca8524cfae6f4c52b430b
harleyjj <harleyyesm@gmail.com>
2018-06-05 13:02:39 -0700

    created find note by id using Note.findById

--- scratch/queries.js
+++ scratch/queries.js
@@ -5,22 +5,41 @@ const { MONGODB_URI } = require('../config');
 
 const Note = require('../models/note');
 
+// mongoose.connect(MONGODB_URI)
+//   .then(() => {
+//     const searchTerm = 'lady gaga';
+//     let filter = {};
+
+//     if (searchTerm) {
+//       filter.title = { $regex: searchTerm };
+//     }
+
+//     return Note.find(filter).sort({ updatedAt: 'desc' });
+//   })    
+//   .then(results => {
+//     console.log(results);
+//   })
+//   .then(() => {
+//     return mongoose.disconnect();
+//   })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
+
 mongoose.connect(MONGODB_URI)
   .then(() => {
-    const searchTerm = 'lady gaga';
-    let filter = {};
-
-    if (searchTerm) {
-      filter.title = { $regex: searchTerm };
-    }
-
-    return Note.find(filter).sort({ updatedAt: 'desc' });
-  })    
+    return Note.find()
+      .then(results => {
+        return Note.findById(results[0]._id);
+      });
+    //return Note.findById(1);
+  })   
   .then(results => {
     console.log(results);
   })
   .then(() => {
-    return mongoose.disconnect()
+    return mongoose.disconnect();
   })
   .catch(err => {
     console.error(`ERROR: ${err.message}`);
