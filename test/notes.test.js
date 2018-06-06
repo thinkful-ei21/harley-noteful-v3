9074e0b27368f0014c0cf817b1a5ddb4b828a8dd
harleyjj <harleyyesm@gmail.com>
2018-06-06 13:00:08 -0700

    test if get returns all notes

--- test/notes.test.js
+++ test/notes.test.js
@@ -17,20 +17,47 @@ chai.use(chaiHttp);
 describe('Notes API resource', function() {
 
   before(function () {
+    console.log('before');
     return mongoose.connect(TEST_MONGODB_URI)
       .then(() => mongoose.connection.db.dropDatabase());
   });
     
   beforeEach(function () {
+    console.log('beforeEach');
     return Note.insertMany(seedNotes);
   });
     
   afterEach(function () {
+    console.log('afterEach');
     return mongoose.connection.db.dropDatabase();
   });
     
   after(function () {
+    console.log('after');
     return mongoose.disconnect();
+  });
+
+  describe('GET /api/notes', function () {
+
+    it('should return all existing notes', function() {
+      // 1) Call the database **and** the API
+      // 2) Wait for both promises to resolve using `Promise.all`
+      return Promise.all([
+        Note.find(),
+        chai.request(app).get('/api/notes')
+      ])
+      // 3) then compare database results to API response
+        .then(([data, res]) => {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+          expect(res.body).to.have.length(data.length);
+        });
+    });
+
+    
+    
   });
     
 });
+
