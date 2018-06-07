6e76771573b64d788c2da23c90620ba221c01b63
harleyjj <harleyyesm@gmail.com>
2018-06-07 11:56:41 -0700

    modified and ran seed-database.js util

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -4,14 +4,23 @@ const mongoose = require('mongoose');
 
 const { MONGODB_URI } = require('../config');
 const Note = require('../models/note');
+const Folder = require('../models/folder');
 
+const seedFolders = require('../db/seed/folders');
 const seedNotes = require('../db/seed/notes');
 
 mongoose.connect(MONGODB_URI)
   .then(() => mongoose.connection.db.dropDatabase())
-  .then(() => Note.insertMany(seedNotes))
+  .then(() => {
+    return Promise.all([
+      Note.insertMany(seedNotes),
+      Folder.insertMany(seedFolders),
+      Folder.createIndexes(),
+    ]);
+  })
   .then(results => {
-    console.info(`Inserted ${results.length} Notes`);
+    console.info(`Inserted ${results[0].length} Notes`);
+    console.info(`Inserted ${results[1].length} Folders`);
   })
   .then(() => mongoose.disconnect())
   .catch(err => {
